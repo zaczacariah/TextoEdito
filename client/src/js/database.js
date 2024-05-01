956bb21da0566d3a7b9c428fca571ca4d9878831
zaczacariah <150572949+zaczacariah@users.noreply.github.com>
2024-05-01 20:41:59 +0930

    Compeleting src files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,37 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("Post to the database");
+
+  const contentDb = await openDB('jate', 1);
+
+  const tx = contentDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.add({ content });
+
+  const result = await request;
+  console.log('Data saved to the database', result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("Get from the database");
+
+  const contentDb = await openDB('jate', 1);
+
+  const tx = contentDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return '';
+};
 
 initdb();
